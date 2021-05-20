6796f4f42f39c2cb68b6953e34d8502cb33562ba
Dmitriy Komar <dmitriyko@playtika.com>
2021-05-20 19:49:05 +0200

    feat: implement logic of task_3_p1_n1

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,6 +5,37 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
+    
+    NSDictionary* monthDict = @{@1: @"January",
+                                @2: @"February",
+                                @3: @"March",
+                                @4: @"April",
+                                @5: @"May",
+                                @6: @"June",
+                                @7: @"July",
+                                @8: @"August",
+                                @9: @"September",
+                                @10: @"October",
+                                @11: @"November",
+                                @12: @"December"
+    };
+    
+    for (id key in monthDict) {
+        if ([key intValue] == monthNumber){
+            return [monthDict objectForKey:key];
+        }
+    }
+    
+    /*
+     for(id key in myDict) {
+         id value = [myDict objectForKey:key];
+         [value doStuff];
+     }
+     */
+    
+    
+    
+    
     return nil;
 }
 
